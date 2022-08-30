1201276e61d792ed1cf31a1d7096e9a33f6a003e
MonteroDav <i1920146@continental.edu.pe>
2022-08-30 01:15:16 -0500

    cambios

--- qerys.js
+++ qerys.js
@@ -7,160 +7,99 @@ const cursos = data.cursos;
 const notascursos = data.notascursos;
 
 
-// /////////////////////////ver todos los alumnos:
-
-
-// const mostrartotodoslosAlumnos=() =>{
-//  console.log(alumnos);
-// }
-
-// mostrartotodoslosAlumnos();
-
-
-
-/////////////////////////mostrar todos los profesores
-
-
-// const mostartodoslosProfesores=() =>{
-
-//     profesores.forEach(prof =>{
-//         console.log("profesor:"+prof.nombres )
-//     })
-// }
-
-// mostartodoslosProfesores();
-
-
-/////////////////////////mostrar todos los salones
-
-
-// const mostrartodosloSalones=()=>{
-//     salones.forEach(salon=> {
-//             console.log("salon:" + salon.nombre)
-//         }
-//     )
-// }
-
-// mostrartodosloSalones();
-
-
-
-/////////////////////////mostrar todos los cursos
-
-// const mostrartodoslosCursos=()=>{
-
-//     cursos.forEach(curso => {
-//         console.log("curso:" + curso.nombre)
-
-//     });
-// }
-
-// mostrartodoslosCursos();
-
-
-
-/////////////////////////mostrar un alumno
-
-// const mostrarUnAlumno=()=>{
-//     const resultado =  alumnos.filter((alumno => alumno.nombres ));
-//     if (resultado.length <= 0) {
-//         return console.log("no se encontraron resultados")
-//     }
-//     return console.log(resultado[2]) ;
-
-// }
-// mostrarUnAlumno();
-
-
-/////////////////////////mostrar un profesor
-
-//  const mostarUnProfesor=() =>{
-
-//     const resultado =  profesores.filter((profesor => profesor.nombres ));
-//     if (resultado.length <= 0) {
-//         return console.log("no se encontraron resultados")
-//     }
-//     return console.log(resultado[2]) ;
-//     }
-
-//     mostarUnProfesor();
-
-
-
-/////////////////////////mostrar un salon
-
-//     const mostrarUnSalon=()=>{
-//         const resultado =  salones.filter((salon => salon.nombres ));
-//         if (resultado.length <= 0) {
-//             return console.log("no se encontraron resultados")
-//         }
-//         return console.log(resultado[2]) ;
-//     }
-
-//     mostrarUnSalon();
-
-
-
-
-
-///////////////////////// al ver 1 alumno:
-		// debo ver, su salon
-		// debo ver sus notas
-		// debo ver sus datos personales
-
-// const mostardatoAlumno=(idalumno) =>{
-// alumnos.forEach(al => {
-//   if (al.id  === idalumno) {
-//     notascursos.forEach(nota => {
-//         if (nota.idalumno === idalumno) {
-//             console.log(" Notas de cursos: mat ing alge fisi quiimica" + "  "+  nota.promedio )
-//         }
-//     });
-//     console.log( "Nombres:"+ al.nombres ,
-//                  "salon:" + al.salon,
-//                  )
-//   }   
-// });
-// }
-// mostardatoAlumno("a1");
-
-
-///////////////////////// al ver 1 profesor
-// al ver un profesor debo ver:
-// 		sus datos personales
-// 		los cursos que enseña
-// 		el salon asignado
-	
-//    mostrarinfoDeMaestro=( idprofesor)=>{
-// profesores.forEach(prof => {
-// if (prof.id=== idprofesor ) {
-//     console.log("nombre del profesor:" + prof.nombres,
-//                  "cursos: "+ prof.cursos ,
-//                  "salon asignado" + prof.salon )
-// }
-// });
-//    }
-//    mostrarinfoDeMaestro("p2");
-
-//////######/////////////////// al ver 1 salon
-
-// al ver un salon debo ver:
-// 		datos del salon
-// 		que alumnos estan en el salon
-// 		que profesor está asignado
-
-const  informaciondeSalon=(idsalon) => {
-
-	salones.filter(s => {
-		idsalon  == s.id ? console.log("nombre de salon" + s.nombre,
-		                                                    ) : 0 
-	alumnos.forEach(a => { 
-		s.idalumno === a.id  ? console.log("alumno" + a.nombres ) : 0
-		console.log(s.id)
-	});
-	});
+const mostrartotodoslosAlumnos=() =>{
+ return console.log(alumnos);
 }
 
-informaciondeSalon("s1")
+const mostartodoslosProfesores=() =>{
+
+    profesores.forEach(prof =>{
+        return console.log("profesor:"+prof.nombres )
+    })
+}
+
+const mostrartodosloSalones=()=>{
+    salones.forEach(salon=> {
+            return console.log("salon:" + salon.nombre)
+        }
+    )
+}
+
+const mostrartodoslosCursos=()=>{
+
+    cursos.forEach(curso => {
+        return console.log("curso:" + curso.nombre)
+
+    });
+}
+
+const mostrarUnAlumno=()=>{
+    const resultado =  alumnos.filter((alumno => alumno.nombres ));
+    if (resultado.length <= 0) {
+        return console.log("no se encontraron resultados")
+    }
+    return console.log(resultado[2]) ;
+
+}
+
+ const mostarUnProfesor=() =>{
+
+    const resultado =  profesores.filter((profesor => profesor.nombres ));
+    if (resultado.length <= 0) {
+        return console.log("no se encontraron resultados")
+    }
+    return console.log(resultado[2]) ;
+    }
+
+    const mostrarUnSalon=()=>{
+        const resultado =  salones.filter((salon => salon.nombres ));
+        if (resultado.length <= 0) {
+            return console.log("no se encontraron resultados")
+        }
+        return console.log(resultado[2]) ;
+    }
+
+const mostardatoAlumno=(idalumno) =>{
+alumnos.forEach(al => {
+  if (al.id  === idalumno) {
+    notascursos.forEach(nota => {
+        if (nota.idalumno === idalumno) {
+            console.log(" Notas de cursos: mat ing alge fisi quiimica" + "  "+  nota.promedio )
+        }
+    });
+    return console.log( "Nombres:"+ al.nombres ,
+                 "salon:" + al.salon,
+                 )
+  }   
+});
+}
+	
+   mostrarinfoDeMaestro=( idprofesor)=>{
+profesores.forEach(prof => {
+if (prof.id=== idprofesor ) {
+    return console.log("nombre del profesor:" + prof.nombres,
+                 "cursos: "+ prof.cursos ,
+                 "salon asignado" + prof.salon )
+}
+});
+   }
 
 
+const mostrarAlumosSalon = (nombreSalon) => { 
+
+    const salon = salones.filter(e => e.nombre === nombreSalon);
+    if(salon.length <= 0) return console.log("no se encontro salon")
+    
+    const resultado = salon[0].idalumno.map((value)=>{ 
+        return value = alumnos.find(al => al.id === value)
+    });
+
+    console.log(resultado)
+    return resultado
+} 
+
+
+
+module.exports = {
+    mostrarAlumosSalon : mostrarAlumosSalon
+}
